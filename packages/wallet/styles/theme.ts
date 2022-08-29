9f2303e6a0c142e4a5b764f43f555b9a976f978b
gtg7784 <tae.gun7784@gmail.com>
2022-08-29 21:21:53 +0900

    chore(wallet): 테마에 attributes 정의

--- packages/wallet/styles/theme.ts
+++ packages/wallet/styles/theme.ts
@@ -1,5 +1,53 @@
 const theme = {
-
+  colors: {
+    white: '#FFFFFF',
+    bluegrey: {
+      dark: '#313A45',
+      main: '#465365',
+      sub: '#737E91',
+      light: '#D7DBE0',
+      pale: '#F3F4F6',
+    },
+    darkgrey: {
+      light: '#A1B2BE',
+      pale: '#919DB6',
+      main: '#3A4044',
+      sub: '#748089',
+    },
+    mediumgrey: {
+      main: '#D9E0E5',
+      dark: '#B9C5CE',
+      sub: '#EAEEF1',
+    },
+    lightgrey: {
+      main: '#FAFBFC',
+    },
+    primary: {
+      main: '#195DEE',
+      pale: '#F0F5FF',
+    },
+  },
+  evaluation: {
+    level5: '0px 10px 15px rgba(54, 62, 76, 0.3), 0px 10px 15px rgba(54, 62, 76, 0.1), 0px 15px 40px rgba(54, 62, 76, 0.2)',
+  },
+  spacing: {
+    small: {
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+    },
+    medium: {
+      1: '16px',
+      2: '32px',
+      3: '48px',
+      4: '64px',
+      5: '80px',
+      6: '96px',
+    },
+  },
 };
 
 type ThemeType = typeof theme;
