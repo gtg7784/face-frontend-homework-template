288d80f4e92820fb60ea90119d521b32fd25b0c7
gtg7784 <tae.gun7784@gmail.com>
2022-08-29 22:09:57 +0900

    chore(wallet): `storybook/main` 수정
    - stories 룰에 `../**/*stories.@(js|jsx|ts|tsx)` 추가
    - `storybook-addon-next` 추가
    - `typescript reactDocgen` 비활성화
    - webpack module rules 에 .svg 파일은 `@svgr/webpack` 으로 로드하도록 추가

--- packages/wallet/.storybook/main.js
+++ packages/wallet/.storybook/main.js
@@ -1,15 +1,41 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    "../**/*.stories.mdx",
+    "../**/*stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    'storybook-addon-next'
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
+  },
+  typescript: {
+    reactDocgen: false
+  },
+  webpackFinal: async (config) => {
+    config.resolve.modules.push(path.resolve(__dirname, '../'));
+
+    const rules = config.module.rules;
+    const fileLoaderRule = rules.find((rule) => rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+
+    rules.unshift({
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+        },
+      }],
+    })
+
+    return config
+  },
+};
