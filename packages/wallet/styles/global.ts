621af10902fa7385fa0d06d927d23f7ac743d36f
gtg7784 <tae.gun7784@gmail.com>
2022-08-29 20:23:26 +0900

    chore(wallet): `styles/global` 추가, globalStyle 작성

---
+++ packages/wallet/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Inter', sans-serif;
+    font-size: 16px;
+  }
+  html, body, #__next, #root {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;
