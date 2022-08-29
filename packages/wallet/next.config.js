84d174e203ea5c16fe3f77233626fb28744613c7
gtg7784 <tae.gun7784@gmail.com>
2022-08-29 21:24:33 +0900

    chore(wallet): `next.config.js` 수정 - compiler 옵션에 styledComponent 활성화 - webpack module rules 에 .svg 파일은 `@svgr/webpack` 으로 로드하도록 추가

--- packages/wallet/next.config.js
+++ packages/wallet/next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+        },
+      }],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
