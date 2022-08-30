86c4dfea7425381db53fae2acc5d73073e27ab50
gtg7784 <tae.gun7784@gmail.com>
2022-08-30 23:44:49 +0900

    chore(wallet): `atom/Modal` 컴포넌트 구현

---
+++ packages/wallet/components/atom/Modal/index.tsx
@@ -0,0 +1,56 @@
+import { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import ReactModal from 'react-modal';
+
+interface IProps {
+  isOpen: boolean;
+}
+
+const Modal = ({ children, ...props }: PropsWithChildren<IProps>) => {
+  ReactModal.setAppElement('#root');
+
+  return (
+    <ReactModal
+      className="_"
+      overlayClassName="_"
+      contentElement={ContentElement}
+      overlayElement={OverlayElement}
+      {...props}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+const ContentElement = (props: React.ComponentPropsWithRef<'div'>, children: React.ReactNode) => (
+  <StyledContentElement {...props}>{children}</StyledContentElement>
+);
+
+const OverlayElement = (props: React.ComponentPropsWithRef<'div'>, children: React.ReactNode) => (
+  <StyledOverlayElement {...props}>{children}</StyledOverlayElement>
+);
+
+const StyledContentElement = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  overflow: auto;
+  border-radius: 16px;
+  outline: none;
+  padding: 36px; // design system 기준 spacing 몇인지 모르겠음
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: ${({ theme }) => theme.evaluation.level5};
+`;
+
+const StyledOverlayElement = styled.div`
+  position: fixed;
+  inset: 0px;
+  // bacground 의 마지막 59 -> 0.35
+  background: ${({ theme }) => theme.colors.darkgrey.dark}59;
+`;
+
+export default Modal;
