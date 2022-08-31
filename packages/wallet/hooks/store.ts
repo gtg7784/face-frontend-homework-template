766623d22425a2df41e76367d625cb9b5a461b95
gtg7784 <tae.gun7784@gmail.com>
2022-08-31 23:01:58 +0900

    chore(wallet): `hooks/store` 작성, redux 스토어에 접근 가능한 custom hook 입니다

---
+++ packages/wallet/hooks/store.ts
@@ -0,0 +1,7 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { AppState, AppDispatch } from 'store';
+
+const useAppDispatch = () => useDispatch<AppDispatch>();
+const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+
+export { useAppDispatch, useAppSelector };
