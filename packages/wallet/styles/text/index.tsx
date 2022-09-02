f579c181f293dcd15327e3abbe41999b7ec2d78c
gtg7784 <tae.gun7784@gmail.com>
2022-09-02 21:58:44 +0900

    chore(wallet): `styles/text` Text 공유 style 작성

---
+++ packages/wallet/styles/text/index.tsx
@@ -0,0 +1,80 @@
+import { css } from 'styled-components';
+
+export const h3 = css`
+  width: 100%;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 36px;
+`;
+
+export const h5 = css`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const body2Bold = css`
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 22px;
+  letter-spacing: 0.005em;
+`;
+
+export const body1Regular = css`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.001em;
+`;
+
+export const body2Regular = css`
+  width: 100%;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  letter-spacing: 0.005em;
+`;
+
+export const body3Regular = css`
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 19px;
+  letter-spacing: 0.005em;
+`;
+
+export const body2Medium = css`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 22px;
+  letter-spacing: 0.005em;
+`;
+
+export const body3Medium = css`
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 19px;
+  letter-spacing: 0.005em;
+`;
+
+export const captionRegular = css`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0.01em;
+`;
+
+export const captionMedium = css`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 18px;
+  letter-spacing: 0.01em;
+`;
+
+export const btn2 = css`
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 20px;
+  letter-spacing: 0.04em;
+`;
