0897c0e89f7f39507f024b7889eca3d73fbf9510
gtg7784 <tae.gun7784@gmail.com>
2022-09-02 22:21:49 +0900

    chore(wallet): `organism/RegisterModal` 작성

---
+++ packages/wallet/components/organism/RegisterModal/index.tsx
@@ -0,0 +1,286 @@
+import {
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react';
+import { FieldErrors, UseFormRegister, UseFormWatch } from 'react-hook-form';
+import Modal from 'components/atom/Modal';
+import ModalHeader from 'components/molecule/ModalHeader';
+import FaceLogo from 'assets/face-logo.svg';
+import { useAppDispatch, useAppSelector } from 'hooks/store';
+import { RegisterFields } from 'interfaces/form';
+import { setRegisterStage } from 'store/modules/modal';
+import useInterval from 'hooks/useInterval';
+import Badge from 'components/atom/Badge';
+import {
+  SubTitle,
+  SubTitleEmailPlaceholder,
+  SecuredByContainer,
+  SecuredByText,
+  StyledButton,
+  StyledEditIcon,
+  StyledInput,
+  SuccessSubTitle,
+  SuccessTitle,
+  Title,
+  ErrorMessage,
+  TimerText,
+  BadgeContainer,
+  TermsText,
+  TermsTextLink,
+  ResendCodeText,
+  ResendCodeTextLink,
+  StyledVisibilityIcon,
+} from './styles';
+
+interface IProps {
+  isOpen: boolean;
+  onClose: () => void;
+  serviceSymbol: React.ReactElement;
+  initialStage?: 'login' | 'verification' | 'password' | 'success';
+}
+
+interface IModalContentProps {
+  isButtonLoading?: boolean;
+  errors?: FieldErrors<RegisterFields>
+  onClickButton: () => void;
+  register: UseFormRegister<RegisterFields>;
+  watch?: UseFormWatch<RegisterFields>;
+}
+
+const RegisterModal = ({
+  isOpen,
+  onClose,
+  serviceSymbol,
+  initialStage = 'login',
+  children,
+}: PropsWithChildren<IProps>) => {
+  const dispatch = useAppDispatch();
+  const { registerStage: stage } = useAppSelector((state) => state.modal);
+  const [isServiceSymbolCentered, setIsServiceSymbolCenterd] = useState(false);
+
+  useEffect(() => {
+    setIsServiceSymbolCenterd(stage === 'success');
+  }, [stage]);
+
+  useEffect(() => {
+    if (!isOpen) {
+      dispatch(setRegisterStage(initialStage));
+    }
+  }, [dispatch, isOpen, initialStage]);
+
+  return (
+    <Modal isOpen={isOpen}>
+      <ModalHeader
+        serviceSymbol={serviceSymbol}
+        onClose={onClose}
+        isServiceSymbolCentered={isServiceSymbolCentered}
+      />
+      {children}
+    </Modal>
+  );
+};
+const Login = ({
+  isButtonLoading,
+  errors,
+  onClickButton,
+  register,
+  watch,
+}: IModalContentProps) => {
+  const { registerStage: stage } = useAppSelector((state) => state.modal);
+  if (stage !== 'login' || !watch || !errors) {
+    return null;
+  }
+
+  return (
+    <>
+      <Title>Connect Face Wallet</Title>
+      <SubTitle>Enter the world of Web3 with Face</SubTitle>
+      <StyledInput
+        placeholder="satoshi@facewallet.xyz"
+        {...register('email', { required: true })}
+        type="email"
+      />
+      {errors.email?.message && (
+        <ErrorMessage>{errors.email?.message}</ErrorMessage>
+      )}
+      <StyledButton onClick={onClickButton} disabled={!watch('email')} isLoading={isButtonLoading}>
+        Sign up / Login
+      </StyledButton>
+      <TermsText>
+        By clicking the button, I acknowledge that I have read and agree to Face Wallet’s
+        {' '}
+        <TermsTextLink>Terms of Service</TermsTextLink>
+        ,
+        {' '}
+        <TermsTextLink>Privacy Policy</TermsTextLink>
+        {' '}
+        and
+        {' '}
+        <TermsTextLink>Cookie Policy</TermsTextLink>
+        {' '}
+        in their entirety.
+      </TermsText>
+      <SecuredByContainer>
+        <SecuredByText>Secured By</SecuredByText>
+        <FaceLogo width={99} height={12} />
+      </SecuredByContainer>
+    </>
+  );
+};
+
+const Verification = ({
+  onClickButton,
+  register,
+  errors,
+}: IModalContentProps) => {
+  const dispatch = useAppDispatch();
+  const { registerStage: stage } = useAppSelector((state) => state.modal);
+  const [timerSecond, setTimerSecond] = useState<number>(5 * 60);
+
+  useInterval(() => {
+    setTimerSecond((prev) => (
+      prev - 1 > 0
+        ? prev - 1
+        : 0
+    ));
+  }, stage === 'verification' ? 1000 : null);
+
+  if (stage !== 'verification' || !errors) {
+    return null;
+  }
+
+  const onclickEmailInput = () => {
+    dispatch(setRegisterStage('login'));
+  };
+
+  const onClickResendVerification = () => {
+    setTimerSecond(5 * 60);
+    onClickButton();
+  };
+
+  return (
+    <>
+      <Title>Email Verification</Title>
+      <SubTitle>Enter code sent to</SubTitle>
+      <StyledInput disabled type="email" {...register('email')} onClick={onclickEmailInput}>
+        <StyledEditIcon />
+      </StyledInput>
+      {
+        timerSecond > 0
+          ? (
+            <StyledInput placeholder="Enter verification code" {...register('verificationCode')}>
+              <TimerText>
+                {Math.floor(timerSecond / 60).toString().padStart(2, '0')}
+                :
+                {(timerSecond % 60).toString().padStart(2, '0')}
+              </TimerText>
+            </StyledInput>
+          )
+          : (
+            <StyledButton onClick={onClickResendVerification}>
+              Resend verification code
+            </StyledButton>
+          )
+      }
+      {errors.verificationCode?.message && (
+        <ErrorMessage>{errors.verificationCode?.message}</ErrorMessage>
+      )}
+      <ResendCodeText>
+        Didn’t get a code?
+        {' '}
+        <ResendCodeTextLink onClick={onClickResendVerification}>Click to resend</ResendCodeTextLink>
+        .
+      </ResendCodeText>
+      <SecuredByContainer>
+        <SecuredByText>Secured By</SecuredByText>
+        <FaceLogo width={99} height={12} />
+      </SecuredByContainer>
+    </>
+  );
+};
+
+const Password = ({
+  onClickButton,
+  register,
+  watch,
+}: IModalContentProps) => {
+  const { registerStage: stage } = useAppSelector((state) => state.modal);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isPasswordConfirmationVisible, setIsPasswordConfirmationVisible] = useState(false);
+
+  const onClickPasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const onClickPasswordConfirmationVisibility = () => {
+    setIsPasswordConfirmationVisible((prev) => !prev);
+  };
+
+  if (stage !== 'password' || !watch) {
+    return null;
+  }
+
+  const passwordCondition: Record<string, boolean> = {
+    '8+ chars': watch('password').length >= 8,
+    Uppercase: watch('password').match(/[A-Z]/) !== null,
+    Lowercase: watch('password').match(/[a-z]/) !== null,
+    Number: watch('password').match(/[0-9]/) !== null,
+    'Password Match': watch('password') === watch('passwordConfirmation') && watch('passwordConfirmation').length > 0,
+  };
+
+  return (
+    <>
+      <Title>Password</Title>
+      <SubTitle>Almost done setting up your account</SubTitle>
+      <SubTitleEmailPlaceholder>{watch('email')}</SubTitleEmailPlaceholder>
+      <StyledInput placeholder="Enter password" type={isPasswordVisible ? 'text' : 'password'} {...register('password')}>
+        <StyledVisibilityIcon onClick={onClickPasswordVisibility} />
+      </StyledInput>
+      <StyledInput placeholder="Re-enter password" type={isPasswordConfirmationVisible ? 'text' : 'password'} {...register('passwordConfirmation')} isMultiple>
+        <StyledVisibilityIcon onClick={onClickPasswordConfirmationVisibility} />
+      </StyledInput>
+      <StyledButton
+        onClick={onClickButton}
+        disabled={!Object.values(passwordCondition).every((v) => v)}
+      >
+        Sign up
+      </StyledButton>
+      <BadgeContainer>
+        {Object.keys(passwordCondition).map((key) => (
+          <Badge key={key} isValid={passwordCondition[key]} text={key} />
+        ))}
+      </BadgeContainer>
+      <SecuredByContainer>
+        <SecuredByText>Secured By</SecuredByText>
+        <FaceLogo width={99} height={12} />
+      </SecuredByContainer>
+    </>
+  );
+};
+
+const Success = () => {
+  const { registerStage: stage } = useAppSelector((state) => state.modal);
+
+  if (stage !== 'success') {
+    return null;
+  }
+
+  return (
+    <>
+      <SuccessTitle>Success!</SuccessTitle>
+      <SuccessSubTitle>Your account ready for use</SuccessSubTitle>
+      <SecuredByContainer>
+        <SecuredByText>Secured By</SecuredByText>
+        <FaceLogo width={99} height={12} />
+      </SecuredByContainer>
+    </>
+  );
+};
+
+RegisterModal.Login = Login;
+RegisterModal.Verification = Verification;
+RegisterModal.Password = Password;
+RegisterModal.Success = Success;
+
+export default RegisterModal;
---
+++ packages/wallet/components/organism/RegisterModal/styles.tsx
@@ -0,0 +1,124 @@
+import styled from 'styled-components';
+import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import Input from 'components/atom/Input';
+import Button from 'components/atom/Button';
+import * as textStyle from 'styles/text';
+
+export const Title = styled.h1`
+  width: 100%;
+  ${textStyle.h3}
+  color: ${({ theme }) => theme.colors.bluegrey.dark};
+  margin-top: ${({ theme }) => theme.spacing.small['2']};
+  margin-bottom: 0;
+`;
+
+export const SubTitle = styled.p`
+  width: 100%;
+  ${textStyle.body2Regular}
+  color: ${({ theme }) => theme.colors.bluegrey.main};
+  margin: 0;
+  letter-spacing: 0.005em;
+`;
+
+export const SecuredByContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 19px;
+  margin-top: ${({ theme }) => theme.spacing.medium['2']};
+`;
+
+export const SecuredByText = styled.span`
+  ${textStyle.body3Medium}
+  color: ${({ theme }) => theme.colors.mediumgrey.dark};
+  margin-right: ${({ theme }) => theme.spacing.small['1']};
+`;
+
+export const SubTitleEmailPlaceholder = styled.p`
+  width: 100%;
+  ${textStyle.body2Bold}
+  color: ${({ theme }) => theme.colors.bluegrey.main};
+  margin: 0;
+`;
+
+export const SuccessTitle = styled(Title)`
+  text-align: center;
+`;
+
+export const SuccessSubTitle = styled(SubTitle)`
+  text-align: center;
+`;
+
+export const StyledInput = styled(Input)<{ isMultiple?: boolean }>`
+  margin-top: ${({ theme, isMultiple }) => (isMultiple ? theme.spacing.small['2'] : theme.spacing.small['6'])};
+`;
+
+export const StyledButton = styled(Button)`
+  margin-top: ${({ theme }) => theme.spacing.small['6']};
+`;
+
+export const StyledEditIcon = styled(EditIcon)`
+  width: 16px;
+  height: 16px;
+  color: ${({ theme }) => theme.colors.bluegrey.sub};
+`;
+
+export const StyledVisibilityIcon = styled(VisibilityIcon)`
+  width: 16px;
+  height: 16px;
+  color: ${({ theme }) => theme.colors.bluegrey.dark};
+`;
+
+export const TermsText = styled.p`
+  width: 288px;
+  ${textStyle.captionRegular}
+  color: ${({ theme }) => theme.colors.darkgrey.pale};
+  margin-top: ${({ theme }) => theme.spacing.small['4']};
+  margin-bottom: 0;
+`;
+
+export const TermsTextLink = styled.a`
+  ${textStyle.captionRegular}
+  color: ${({ theme }) => theme.colors.primary.main};
+  text-decoration: none;
+`;
+
+export const ResendCodeText = styled.p`
+  width: 288px;
+  text-align: center;
+  ${textStyle.body3Regular}
+  color: ${({ theme }) => theme.colors.darkgrey.pale};
+  margin-top: ${({ theme }) => theme.spacing.small['4']};
+  margin-bottom: 0;
+`;
+
+export const ResendCodeTextLink = styled.a`
+  ${textStyle.body3Regular}
+  color: ${({ theme }) => theme.colors.primary.main};
+  text-decoration: none;
+`;
+
+export const ErrorMessage = styled.p`
+  width: 288px;
+  text-align: left;
+  ${textStyle.body2Regular}
+  color: ${({ theme }) => theme.colors.red.dark};
+  margin-top: ${({ theme }) => theme.spacing.small['2']};
+  margin-bottom: 0;
+`;
+
+export const TimerText = styled.span`
+  ${textStyle.body1Regular}
+  color: ${({ theme }) => theme.colors.primary.main};
+`;
+
+export const BadgeContainer = styled.div`
+  width: 288px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: ${({ theme }) => theme.spacing.small['1']};
+  margin-top: ${({ theme }) => theme.spacing.small['2']};
+`;
