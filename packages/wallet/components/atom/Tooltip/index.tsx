be88c138088fee7cb9bff64484373c5f3ec01149
gtg7784 <tae.gun7784@gmail.com>
2022-09-04 15:58:07 +0900

    chore(wallet): `atom/Tooltip` 컴포넌트 작성

---
+++ packages/wallet/components/atom/Tooltip/index.tsx
@@ -0,0 +1,45 @@
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import * as textStyle from 'styles/text';
+
+interface IProps {
+  message: string;
+}
+
+const Tooltip = ({ message, children, ...props }: PropsWithChildren<IProps>) => (
+  <Container {...props}>
+    {children}
+    <Message>{message}</Message>
+  </Container>
+);
+
+const Container = styled.div`
+  position: relative;
+  width: fit-content;
+  height: fit-content;
+  cursor: pointer;
+
+  &:hover {
+    & > div {
+      display: block;
+    }
+  }
+`;
+
+const Message = styled.div`
+  position: absolute;
+  display: none;
+  top: 0;
+  left: 50%;
+  transform: ${({ theme }) => `translate(-50%, calc(-100% - ${theme.spacing.small['1']}))`};
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 4px;
+  ${textStyle.captionMedium}
+  padding: ${({ theme }) => `6px ${theme.spacing.small['2']}`};
+  background: ${({ theme }) => theme.colors.darkgrey.main};
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export default Tooltip;
+export type { IProps };
