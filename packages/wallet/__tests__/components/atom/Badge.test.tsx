ce0743c7f4c23d2fdb7e9c49fe00aa241882673e
gtg7784 <tae.gun7784@gmail.com>
2022-09-07 22:42:33 +0900

    test(wallet): `atom/Badge` 컴포넌트 테스트 작성

---
+++ packages/wallet/__tests__/components/atom/Badge.test.tsx
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker';
+import Badge from 'components/atom/Badge';
+import theme from 'styles/theme';
+import { renderWithTheme } from 'testing-library';
+
+describe('components/atom/Badge', () => {
+  it('should render', () => {
+    const text = faker.lorem.word();
+    const isValid = faker.datatype.boolean();
+    const { queryByTestId } = renderWithTheme(<Badge text={text} isValid={isValid} />, {});
+
+    expect(queryByTestId('components/atom/Badge')).toBeInTheDocument();
+    expect(queryByTestId('components/atom/Badge')).toHaveTextContent(text);
+  });
+
+  it('should render with valid style', () => {
+    const text = faker.lorem.word();
+    const isValid = true;
+    const { queryByTestId } = renderWithTheme(<Badge text={text} isValid={isValid} />, {});
+
+    expect(queryByTestId('components/atom/Badge')).toHaveStyle(`
+      background: ${theme.colors.primary.pale};
+      color: ${theme.colors.primary.main};
+    `);
+  });
+
+  it('should render with invalid style', () => {
+    const text = faker.lorem.word();
+    const isValid = false;
+    const { queryByTestId } = renderWithTheme(<Badge text={text} isValid={isValid} />, {});
+
+    expect(queryByTestId('components/atom/Badge')).toHaveStyle(`
+      background: ${theme.colors.bluegrey.pale};
+      color: ${theme.colors.bluegrey.dark};
+    `);
+  });
+});
