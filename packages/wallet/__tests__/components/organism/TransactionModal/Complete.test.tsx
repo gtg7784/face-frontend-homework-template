4aa24ae9c78110bc84942ae7665ddc606fd95761
gtg7784 <tae.gun7784@gmail.com>
2022-09-10 10:06:44 +0900

    test(wallet): `organism/TransactionModal.Complete` 테스트 작성

---
+++ packages/wallet/__tests__/components/organism/TransactionModal/Complete.test.tsx
@@ -0,0 +1,79 @@
+import TransactionModal from 'components/organism/TransactionModal';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { renderWithTheme } from 'testing-library';
+
+const mockStore = configureStore();
+
+describe('components/organism/TransactionModal.Complete', () => {
+  it('should render', () => {
+    const initialState = {
+      modal: {
+        transactionStage: 'complete',
+      },
+    };
+
+    const store = mockStore(initialState);
+
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    );
+
+    const amount = '10';
+    const to = '0x5D5AA22d586b7904B2D348fad79b1eA3D0eeb520';
+    const fee = '0.1';
+    const hash = '0x5D5AA22d586b7904B2D348fad79b1eA3D0eeb520';
+
+    const { queryByText } = renderWithTheme(
+      <TransactionModal.Complete
+        amount={amount}
+        to={to}
+        fee={fee}
+        hash={hash}
+      />,
+      { wrapper },
+    );
+
+    expect(queryByText('Complete!')).toBeInTheDocument();
+    expect(queryByText(`${amount} ETH`)).toBeInTheDocument();
+    expect(queryByText(`${to.slice(0, 6)}...${to.slice(-6)}`)).toBeInTheDocument();
+    expect(queryByText(`${fee} ETH`)).toBeInTheDocument();
+    expect(queryByText(`${parseFloat(amount) + parseFloat(fee)}`)).toBeInTheDocument();
+    expect(queryByText('View on block explorer')).toHaveAttribute('href', `https://ropsten.etherscan.io/tx/${hash}`);
+  });
+
+  it('should not render when stage is not complete', () => {
+    const initialState = {
+      modal: {
+        transactionStage: 'send',
+      },
+    };
+
+    const store = mockStore(initialState);
+
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <Provider store={store}>
+        {children}
+      </Provider>
+    );
+
+    const amount = '10';
+    const to = '0x5D5AA22d586b7904B2D348fad79b1eA3D0eeb520';
+    const fee = '0.1';
+    const hash = '0x5D5AA22d586b7904B2D348fad79b1eA3D0eeb520';
+
+    const { queryByText } = renderWithTheme(
+      <TransactionModal.Complete
+        amount={amount}
+        to={to}
+        fee={fee}
+        hash={hash}
+      />,
+      { wrapper },
+    );
+
+    expect(queryByText('Complete!')).not.toBeInTheDocument();
+  });
+});
